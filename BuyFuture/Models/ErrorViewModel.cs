3adea65ba6ad065ec6a0b5ea1c947fc5f2b4cf38
ujdream <68264701+uj0320@users.noreply.github.com>
2020-07-15 10:30:10 +0800

    Add project files.

---
+++ BuyFuture/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BuyFuture.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
