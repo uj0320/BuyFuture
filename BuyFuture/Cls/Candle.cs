33c9519286277240c5f9a264c72c0e1187eefb1f
PoorWorm <null@mail2000.com.tw>
2020-07-15 11:51:59 +0800

    json 範例

---
+++ BuyFuture/Cls/Candle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuyFuture.Cls
+{
+    public class Candle
+    {
+        public List<string> x { get; set; }
+        public List<decimal> close { get; set; }
+
+
+
+    }
+}
